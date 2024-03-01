a1980367418a06468c6f13d9179d1734c2b1e1d5
Lucas Regalado <lucasregalado@hotmail.com>
2024-03-01 13:48:04 -0300

    configure app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
+import styled from "styled-components";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { FeaturedCategoryPage } from "./pages/FeaturedCategoryPage";
+
 const App = () => {
+  const StyledSection = styled.section`
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  `;
+
   return (
-    <>
+    <StyledSection>
       <Header />
       <FeaturedCategoryPage />
       <Footer />
-    </>
+    </StyledSection>
   );
 };
 
