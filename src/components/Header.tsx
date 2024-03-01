8c3ce4fe81bcacc5d61cb84dd614f16edbd9c015
Lucas Regalado <lucasregalado@hotmail.com>
2024-03-01 13:48:20 -0300

    create footer and header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,18 @@
+import styled from "styled-components";
+
 export const Header = () => {
+  const StyledHeader = styled.header`
+    height: 40px;
+    background: #EEE;
+    display: flex;
+    align-items: center;
+    justify-content: end;
+    padding: 0px 20px;
+    margin-bottom: 20px
+  `;
   return (
-    <div>Header</div>
-  )
-}
+    <StyledHeader>
+      <h1>LOI</h1>
+    </StyledHeader>
+  );
+};
