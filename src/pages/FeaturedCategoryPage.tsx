411e8a25e2eaa27247c0c311005e858975f74041
Lucas Regalado <lucasregalado@hotmail.com>
2024-03-01 14:28:18 -0300

    add loading component

--- src/pages/FeaturedCategoryPage.tsx
+++ src/pages/FeaturedCategoryPage.tsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
+import { Loading } from "../components/Loading";
+
 
 export const FeaturedCategoryPage = () => {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  // useEffect(() => {
+  //   setLoading(true);
+  // }, []);
+
   const StyledFeaturedCategory = styled.section`
     flex: 1;
     padding: 0px 20px;
+    position: relative;
   `;
-  return <StyledFeaturedCategory>FeaturedCategoryPage</StyledFeaturedCategory>;
+  return (
+    <StyledFeaturedCategory>
+      {loading && <Loading/>}
+    </StyledFeaturedCategory>
+  );
 };
